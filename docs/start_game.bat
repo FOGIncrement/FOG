9d0012bad09cdd1f1721a37a831bddd485984924
Eoinknd16 <42419154+Eoinknd16@users.noreply.github.com>
2026-03-02 23:05:46 +0000

    Moved index to root

--- docs/start_game.bat
+++ docs/start_game.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-cd /d "%~dp0"
+cd /d "%~dp0.."
 echo Starting local server at http://localhost:5500/index.html
 start "FOG" http://localhost:5500/index.html
 py -m http.server 5500
